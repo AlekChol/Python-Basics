c8a674c4a3f7e047334ae28a28c8cb775b71303a
AlekChol <alelmabil28@gmail.com>
2024-02-14 10:40:25 +0300

    A program to slice

---
+++ slicing.py
@@ -0,0 +1,13 @@
+'''' 
+A program to perform slicing.
+'''
+word="Holberton"
+#get the first three letters
+word_first_3=word[0:3]
+print("first three letters:{}".format(word_first_3))
+#last 2 letters
+word_last_2=word[-2:]
+print("first three letters:{}".format(word_last_2))
+#middle word
+middle_word=word[3:-3]
+print('middle word:{}'.format(middle_word))
